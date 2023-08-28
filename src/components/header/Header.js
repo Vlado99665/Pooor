569b37d0cf42ed317974b790c6d14bb4c4371f3a
Vlado99665 <ushin99665@gmail.com>
2023-08-28 11:36:12 +0300

    f

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,24 +1,21 @@
 import "./style.css";
 
 const Header = () => {
-    return (
-		<header className="header">
-			<div className="header__wrapper">
-				<h1 className="header__title">
-					<strong>
-						Hi, my name is <em>Yuri</em>
-					</strong>
-					<br />a frontend developer
-				</h1>
-				<div className="header__text">
-					<p>with passion for learning and creating.</p>
-				</div>
-				<a href="#!" className="btn">
-					Download CV
-				</a>
-			</div>
-		</header>
-	);
-}
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <h1 className="header__title">
+          <strong>
+            Hi, my name is <em>Vladislav</em>
+          </strong>
+          <br />a bad boy
+        </h1>
+        <a href="#!" className="btn">
+          Download resume
+        </a>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
